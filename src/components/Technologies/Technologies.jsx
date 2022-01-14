ae1b58d81219706cc51d43bbccee07c3ff2549ce
George Harakis <harakisgeorge@gmail.com>
2022-01-14 13:03:49 +0200

    starting technologies part

---
+++ src/components/Technologies/Technologies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Technologies.css"
+const Technologies = () => {
+   return (
+         <main>
+            <div className='technologies'>
+               <h4>Technologies</h4>
+            </div>
+         </main>
+   )
+}
+
+export default Technologies
