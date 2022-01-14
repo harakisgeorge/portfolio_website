19fc58b37402265a6f08973de63435a67cf03eb3
George Harakis <harakisgeorge@gmail.com>
2022-01-14 12:35:19 +0200

    projects part

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -20,27 +20,64 @@ const Projects = () => {
                      <li>React</li>
                      <li>HTML</li>
                   </ul>
+                  <ul className='code_source-buttons'>
+                     <li><a href="">Code</a></li>
+                     <li><a href="">Source</a></li>
+                  </ul>
                </div>
                <div className='project'>
                   <img src={img} />
                   <h4>Project 2</h4>
-                  <p>Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application -
+                  <p className="description">Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application -
                       from start to finish. The App is called Memories and it is
                       a simple social media app that allows users to post interesting events that happened in their lives.</p>
+                  <p id="stack">Stack</p>
+                  <ul className='stack-list'>
+                     <li>Express</li>
+                     <li>Mongo</li>
+                     <li>React</li>
+                     <li>HTML</li>
+                  </ul>
+                  <ul className='code_source-buttons'>
+                     <li><a href="">Code</a></li>
+                     <li><a href="">Source</a></li>
+                  </ul>
                </div>
                <div className='project'>
                   <img src={img} />
                   <h4>Project 3</h4>
-                  <p>Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application -
+                  <p className="description">Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application -
                       from start to finish. The App is called Memories and it is
                       a simple social media app that allows users to post interesting events that happened in their lives.</p>
+                  <p id="stack">Stack</p>
+                  <ul className='stack-list'>
+                     <li>Express</li>
+                     <li>Mongo</li>
+                     <li>React</li>
+                     <li>HTML</li>
+                  </ul>
+                  <ul className='code_source-buttons'>
+                     <li><a href="">Code</a></li>
+                     <li><a href="">Source</a></li>
+                  </ul>
                </div>
                <div className='project'>
                   <img src={img} />
                   <h4>Project 4</h4>
-                  <p>Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application -
+                  <p className="description">Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application -
                       from start to finish. The App is called Memories and it is
                       a simple social media app that allows users to post interesting events that happened in their lives.</p>
+                  <p id="stack">Stack</p>
+                  <ul className='stack-list'>
+                     <li>Express</li>
+                     <li>Mongo</li>
+                     <li>React</li>
+                     <li>HTML</li>
+                  </ul>
+                  <ul className='code_source-buttons'>
+                     <li><a href="">Code</a></li>
+                     <li><a href="">Source</a></li>
+                  </ul>
                </div>
             </div>
             <div className='underline'></div>
